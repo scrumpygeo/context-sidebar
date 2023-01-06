1cc34cac4220857ce06a3ae70a20f28a2c5a8de4
scrumpygeo <scrumpygeo@yahoo.co.uk>
2023-01-06 11:18:02 +0000

    add modal functionality

--- src/Modal.js
+++ src/Modal.js
@@ -1,11 +1,14 @@
 import { FaTimes } from 'react-icons/fa'
-
+import { useContext } from 'react'
+import { AppContext } from './context'
 const Modal = () => {
+  const { isModalOpen, closeModal } = useContext(AppContext)
+
   return (
-    <div className=" modal-overlay">
+    <div className={`${isModalOpen ? 'show-modal modal-overlay' : 'modal-overlay'}  `}>
       <div className="modal-container">
-        <h3>Modal Content</h3>
-        <button className="close-modal-btn"><FaTimes /></button>
+        <h3>Modal Content 🎈</h3>
+        <button className="close-modal-btn" onClick={closeModal} ><FaTimes /></button>
       </div>
     </div>
   )
