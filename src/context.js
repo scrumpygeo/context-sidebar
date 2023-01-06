883820b614de01ca28dc2920b0b3c97c98acf625
scrumpygeo <scrumpygeo@yahoo.co.uk>
2023-01-06 10:48:12 +0000

    add state and methods to context

--- src/context.js
+++ src/context.js
@@ -3,10 +3,24 @@ import React, { useState, useContext } from 'react'
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  // add state and methods
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true)
+  }
+  const closeSidebar = () => {
+    setIsSidebarOpen(false)
+  }
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
 
   return (
-    <AppContext.Provider>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ isModalOpen, isSidebarOpen, openModal, closeModal, openSidebar, closeSidebar }}>{children}</AppContext.Provider>
   )
 }
 
