e69ed5691c41a97d5f0c8368ebb22dcdf3151813
scrumpygeo <scrumpygeo@yahoo.co.uk>
2023-01-06 10:16:19 +0000

    setup sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import logo from './logo.svg'
+import { FaTimes } from 'react-icons/fa'
+import { social, links } from './linkdata'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside className="show-sidebar sidebar">
+      <div className="sidebar-header">
+        <img src={logo} alt="nasa-logo" className='logo' />
+        <button className="close-btn" ><FaTimes /></button>
+      </div>
+      Sidebar
+    </aside>
   )
 }
 
--- src/linkdata.js
+++ src/linkdata.js
@@ -1,10 +1,15 @@
 import React from "react";
 import {
+  FaBehance,
   FaFacebook,
   FaLinkedin,
   FaTwitter,
   FaSketch,
   FaHome,
+  FaUserFriends,
+  FaFolderOpen,
+  FaCalendarAlt,
+  FaWpforms,  
 } from 'react-icons/fa';
 
 export const links = [
